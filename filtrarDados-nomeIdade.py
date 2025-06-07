565e069aab68a22c911e70ea94842abe4fb07cb9
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-06-07 18:47:45 -0300

    Add files via upload

---
+++ filtrarDados-nomeIdade.py
@@ -0,0 +1,11 @@
+"""
+Faça um algoritmo que filtre os dados de uma lista.
+ Separe os dados somente com letras na lista nomes e os dados somente com números
+ na lista idades
+"""
+dados = ["Pedro", "25", "Maria", "30", "João", "50"]
+nomes = [nome for nome in dados if nome.isalpha()]
+idades = [idade for idade in dados if not idade.isalpha()]
+print("Nomes", nomes)
+print("Idades", idades)
+
