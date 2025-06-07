e63af9ebfc7fe21642a9f722543e889be666d384
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-06-07 18:30:43 -0300

    Update 08valide-username.py

--- 08valide-username.py
+++ 08valide-username.py
@@ -1,10 +1,11 @@
 """
 08valide-username.py
-crie um algoritmo que valide a entrada de dados para criação de usuario,
-de forma a cumprir a condição de receber apanas letras,
-sem espaços ou numeros,
-crie um laço de repetição para que o programa
-volte ao inicio se houver erro
+
+crie um algoritmo que valide a entrada de dados para criação de usuario e ,
+de forma a cumprir a condição de receber apanas letras no usuario,
+sem espaços ou numeros adicionais, e na senha a condição de não conter apenas letras
+crie um laço de repetição para que o programa volte ao inicio se houver erro.
+
 nivel: elementar
 """
 
@@ -15,3 +16,15 @@ while True:
         break
     else:
         print("Username deve conter apenas letras, sem números ou símbolos")
+
+while True:
+    print("\n!! Crie uma senha com letras e outros caracteres !!\n")
+    senha = input("Crie uma senha: ")
+    if senha.isalpha():
+        print("\nSenha fraca! Use outros caracteres além de letras.")
+    else:
+        print("Senha aceita")
+        break
+
+print(f"\nUSER: {username}")
+print(f"SENHA: {senha}")
