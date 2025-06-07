6a7e1fdbb305534da9a45b1d6a9bf279ccf33533
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-06-07 16:46:39 -0300

    Add files via upload

---
+++ valide-username.py
@@ -0,0 +1,15 @@
+"""
+crie um algoritmo que valide a entrada de dados para criação de usuario,
+de forma a cumprir a condição de receber apanas letras,
+sem espaços ou numeros,
+crie um laço de repetição para que o programa
+volte ao inicio se houver erro
+"""
+
+while True:
+    username = input("!! Crie um username (apenas com letras) !!\nUSERNAME: ")
+    if username.isalpha():
+        print("Username aceito!")
+        break
+    else:
+        print("Username deve conter apenas letras, sem números ou símbolos")
