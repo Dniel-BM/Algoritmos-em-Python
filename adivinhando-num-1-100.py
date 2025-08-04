432c893ed77ca4d76c41dc6ae8ac1bfa5952d8ba
danielbm <137958150+Dniel-BM@users.noreply.github.com>
2025-08-04 00:05:29 -0300

    Add files via upload

---
+++ adivinhando-num-1-100.py
@@ -0,0 +1,33 @@
+"""
+Algoritmo que cria um jogo de adivinhação
+onde o usuario tenta advinhar um número sorteado entre 1 e 100
+com número de tentativas definidos
+e dicas ao longo das tentativas revelando se o número é maior ou menor
+"""
+
+import random
+
+numero_sorteado = random.randint(1, 100)
+tentativa = 1
+limite_tentativas = 11
+acertou = False
+
+print("Jogo de Adivinhação")
+print(f"Você tem {limite_tentativas} tentativas para adivinhar o número sorteado entre 1 e 100")
+
+while tentativa < limite_tentativas and not acertou:
+    palpite = int(input(f"Palpite {tentativa}: "))
+    tentativa += 1
+
+    if palpite == numero_sorteado:
+        print(f"Parabéns! Você acertou o número {numero_sorteado} na {tentativa-1} tentativa.")
+        acertou = True
+    elif palpite < numero_sorteado:
+        print("O número é maior")
+    else:
+        print("O número é menor")
+
+if not acertou:
+    print(f"Game Over! O número sorteado era {numero_sorteado}")
+
+
